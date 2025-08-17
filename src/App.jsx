a33184a8b36523eda1566854d6a894b5887e68e9
xiokevc <xiokevc@gmail.com>
2025-08-17 11:49:28 -0400

    update app.jsx routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
+import { Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+import TrackForm from './components/TrackForm';
+
 const App = () => {
-  return <h1>Hello world!</h1>;
+  return (
+    <div className="app-container">
+      <h1>Reactville Jukebox 🎶</h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/add-track" element={<TrackForm />} />
+        <Route path="/edit-track/:trackId" element={<TrackForm />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
